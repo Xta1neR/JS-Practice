ee39b5b3db60250498c76d89cf023f963c62f551
Rituraj <goswami.rituraj03@gmail.com>
2025-04-09 19:47:58 +0530

    first code

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# This is a place where I will be practicing all the concept of Javascript, from basics to Advanced features. 
+## Date : 09th April 2024. 
+## Author : Rituraj Goswami
+
+### Index
+1. A simple application explaining the logic behind the Traffic Lights - trafficLight.js
---
+++ trafficLight.js
@@ -0,0 +1,15 @@
+let light = "green";
+
+function logicTrafficLight() {
+    if (light == "red"){
+        console.log("Stop");
+    }
+    if (light == "yellow"){
+        console.log("Get ready to go");
+    }
+    if (light == "green"){
+        console.log("Go");
+    }
+}
+
+logicTrafficLight();
