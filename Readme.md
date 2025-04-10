6a607d3639e6426acc0eca6848f489c967408157
Rituraj <goswami.rituraj03@gmail.com>
2025-04-10 18:03:22 +0530

    Added week finder

--- Readme.md
+++ Readme.md
@@ -6,3 +6,5 @@
 1. A simple application explaining the logic behind the Traffic Lights - trafficLight.js
 2. Grading System - gradeSystem.js
 3. Popcorn price calculating System - popcornPrice.js
+4. To check weather a good string or not, a good string starts with 'a' and has a length more than 3 - goodString.js
+5. Tell which day of the week is today using switch cases - switchWeekday.js
---
+++ switchWeekday.js
@@ -0,0 +1,25 @@
+let day = "Wednesday";
+
+switch(day) {
+    case "Monday":
+        console.log("Start of the week");
+        break;
+    case "Tuesday":
+        console.log("Second day of the week");
+        break;
+    case "Wednesday":
+        console.log("Third day of the week");
+        break;
+    case "Thursday":
+        console.log("Fourth day of the week");
+        break;
+    case "Friday":
+        console.log("Fifth day of the week");
+        break;
+    case "Saturday":
+        console.log("Sixth day of the week");
+        break;
+    case "Sunday":
+        console.log("Seventh day of the week");
+        break;
+}
