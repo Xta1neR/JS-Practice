5fcabb678310f911eaf5a44dc3fe247eeaa76b4e
Rituraj <goswami.rituraj03@gmail.com>
2025-04-16 19:32:45 +0530

    Learned About While loops

--- Table.js
+++ Table.js
@@ -2,7 +2,14 @@
 let n = prompt("Enter a number");
 num = parseInt(n);
 
-//Printing the table of 5
+//Printing the table of num, FOR LOOP
 for (let i = 1; i<= 10; i++){
     console.log(`${num} * ${i} = ${num*i}`);
+}
+
+//Printing the table of num, WHILE LOOP
+let j = 1;
+while (j <= 10){
+    console.log(`${num} * ${j} = ${num*j}`);
+    j++;
 }
